02f91e29c92503c4761965b071f21bbdd773e1b9
Alex Davis <alex2006hw@gmail.com>
2015-08-28 22:04:56 -0700

    rename nginx to webserver in sample

--- sample/run.sh
+++ sample/run.sh
@@ -1,7 +1,7 @@
-docker run --name nginx -d \
+docker run --name webserver -d \
   -v `pwd`/log:/var/log/nginx \
   -v `pwd`/upload:/var/upload \
   -p 8000:80 -p 8433:443 \
-  -t pointlook/nginx
+  -t pointlook/webserver
 
-docker exec -it nginx bash
+docker exec -it webserver bash
